3a457759ec85bf4a78e3398c426ccc4825430433
pamelabispo3105-hub <pamelabispo3105@gmail.com>
2025-08-22 13:06:56 -0300

    Add files via upload

--- Exercício 1 lição.py
+++ Exercício 1 lição.py
@@ -1,8 +1,6 @@
 # Exercício 1 lição
 
-#Exercício 1: Verificar se um número é positivo
+Número = float (input( "Digite um Número:"))
 
-# Número = float (input( "Digite um Número:"))
-
-# Número > 0:
-#      print ("Positivo")
+if Número > 0:
+     print ("Positivo")
